8dc567a7c6f73da1def7dc38c3644a93d4b597ad
Cris A <caranda@stabilityit.net>
2024-01-31 11:11:51 -0600

    update

--- js/AD_Discovery.js
+++ js/AD_Discovery.js
@@ -2,6 +2,17 @@ function sortTable(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     //table = document.getElementById("myTable");
     table = document.getElementsByTagName("table")[0];
+  
+    //document.getElementsByTagName("TH")[n].style.background = 'green'
+    allTH = document.getElementsByTagName("TH")
+    for (i = 0; i < (allTH.length - 1); i++) {
+      allTH[i].style.background = '#666'
+      allTH[i].innerHTML = (/^\w+/.exec(allTH[i].innerHTML))[0]
+      //console.log("n value: " + n)
+      //console.log("i value: " + i + " data: " + allTH[i].innerHTML)
+    }
+    // Set the background for the TH that was selected
+    allTH[n].style.background = '#8b8282'
     switching = true;
     //Set the sorting direction to ascending:
     dir = "asc"; 
@@ -20,9 +31,10 @@ function sortTable(n) {
         one from current row and one from the next:*/
         x = rows[i].getElementsByTagName("TD")[n];
         y = rows[i + 1].getElementsByTagName("TD")[n];
+        
         /*check if the two rows should switch place,
         based on the direction, asc or desc:*/
-        if (dir == "asc") {
+        if (dir == "asc") { 
           if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
             //if so, mark as a switch and break the loop:
             shouldSwitch= true;
@@ -52,4 +64,9 @@ function sortTable(n) {
         }
       }
     }
-  }
+    if (dir == "asc") {
+        allTH[n].innerHTML = allTH[n].innerHTML + ' &#8593'
+    } else {
+        allTH[n].innerHTML = allTH[n].innerHTML + ' &#8595'
+    }
+}
